cb69689083e66a8e3b21406453b0918203aa76a5
Gopirg07 <gopinathofficial16@gmail.com>
2023-07-05 13:03:17 +0530

    Finished

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 var html = `
-  <h1>Welcome To The Mentor 👨🏻‍🏫 & Student 👨🏻‍🎓 Assigning With The Database  </h1>
-  <p>Create a Mentor : POST - "/mentor" *name email required*</p>
-  <p>Create a Student : POST - "/student" *name age required*</p>
-  <p>Assign a Student For A Mentor : PUT - "/mentor/:mentorId/student/:studentId"</p>
+  <h1>  Mentor 👨🏻‍🏫 & Student 👨🏻‍🎓 Assigning With The Database  </h1>
+  <p>To Create a Mentor : POST - "/mentor" *name email required*</p>
+  <p>To Create a Student : POST - "/student" *name age required*</p>
+  <p>To Assign a Student to Mentor : PUT - "/mentor/:mentorId/student/:studentId"</p>
   <p>Assign Or Change The Mentor for a Student : PUT - "/student/:studentId/mentor/:mentorId"</p>
-  <p>Show all Students for a Mentor : GET - "/mentor/:mentorId/student"</p>
-  <p>Show the previously assigned Mentor for a Student : GET - "/student/:studentId/mentor"</p>
+  <p>show all students for a particular mentor : GET - "/mentor/:mentorId/student"</p>
+  <p>show the previously assigned mentor for a particular student. : GET - "/student/:studentId/mentor"</p>
 `;
 
 /* GET home page. */
